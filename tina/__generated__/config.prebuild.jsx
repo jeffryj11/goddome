44b63c766cc087c2e410cdeae874c0941f3e6163
Jeff Johnson <jeff@odiscom.com>
2026-08-01 02:47:47 -0500

    Replace Decap CMS with TinaCMS schema and build configuration

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,78 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var config_default = defineConfig({
+  branch: process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "images/stories",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "stories",
+        label: "Devotionals & Stories",
+        path: "content/stories",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Category"
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author"
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date"
+          },
+          {
+            type: "string",
+            name: "readTime",
+            label: "Read Time"
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Featured Image"
+          },
+          {
+            type: "string",
+            name: "excerpt",
+            label: "Excerpt",
+            ui: {
+              component: "textarea"
+            }
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
---
+++ tina/__generated__/types.js
@@ -0,0 +1,116 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const StoriesPartsFragmentDoc = gql`
+    fragment StoriesParts on Stories {
+  __typename
+  title
+  category
+  author
+  date
+  readTime
+  image
+  excerpt
+  body
+}
+    `;
+export const StoriesDocument = gql`
+    query stories($relativePath: String!) {
+  stories(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...StoriesParts
+  }
+}
+    ${StoriesPartsFragmentDoc}`;
+export const StoriesConnectionDocument = gql`
+    query storiesConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: StoriesFilter) {
+  storiesConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...StoriesParts
+      }
+    }
+  }
+}
+    ${StoriesPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    stories(variables, options) {
+      return requester(StoriesDocument, variables, options);
+    },
+    storiesConnection(variables, options) {
+      return requester(StoriesConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, options) => {
+    let url = client.apiUrl;
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    }, options);
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "http://localhost:4001/graphql",
+      queries
+    })
+  )
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
